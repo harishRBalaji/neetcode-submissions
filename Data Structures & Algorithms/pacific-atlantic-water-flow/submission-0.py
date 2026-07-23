3af734223376f2d2734d2961f51e4fd7f5373f74
Harish Balaji <48466864+harishRBalaji@users.noreply.github.com>
2026-07-23 10:07:50 -0700

    Add: pacific-atlantic-water-flow - submission-0

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        pacific = set()
+        atlantic = set()
+        ROWS, COLS = len(heights), len(heights[0])
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        def dfs(r, c, visited, prev_height):
+            if ((r, c) in visited or
+                r < 0 or c < 0 or
+                r == ROWS or c == COLS or
+                heights[r][c] < prev_height):
+                return
+            visited.add((r, c))
+            dfs(r + 1, c, visited, heights[r][c])
+            dfs(r - 1, c, visited, heights[r][c])
+            dfs(r, c + 1, visited, heights[r][c])
+            dfs(r, c - 1, visited, heights[r][c])
+            
+        for c in range(COLS):
+            dfs(0, c, pacific, heights[0][c])
+            dfs(ROWS - 1, c, atlantic, heights[ROWS - 1][c])
+        for r in range(ROWS):
+            dfs(r, 0, pacific, heights[r][0])
+            dfs(r, COLS - 1, atlantic, heights[r][COLS - 1])
+
+        result = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) in pacific and (r, c) in atlantic:
+                    result.append([r, c])
+        return result
